e9bcdcbda2a57c5805d3e631bf17d6df95a13c52
Dke76 <156434263+Dke76@users.noreply.github.com>
2024-02-13 11:59:16 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+# txt uploader
+
+# Made By TIGER
+
+
+## DEPLOY TO HEROKU
+
+
+[![Deploy to heroku chacha](https://www.herokucdn.com/deploy/button.svg)](https://dashboard.heroku.com/new?template=https://github.com/Dke76/LPRPDV-tiger-txt-ADVANCED)
---
+++ config.py
@@ -0,0 +1,22 @@
+import os
+
+API_ID = API_ID = 20319884
+API_HASH = os.environ.get("API_HASH", "637e3ba6357aa3ba2f3bf5742e0fd066")
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6552766527:AAEtbRSMbs3J5sPeErnhTVh48uqgwCry52k")
+
+PASS_DB = int(os.environ.get("PASS_DB", "721"))
+
+OWNER = int(os.environ.get("OWNER", 1183124209))
+
+LOG = -1001861014060
+
+try:
+    ADMINS=[]
+    for x in (os.environ.get("ADMINS", "5665231556").split()):
+        ADMINS.append(int(x))
+except ValueError:
+        raise Exception("Your Admins list does not contain valid integers.")
+ADMINS.append(OWNER)
+
+
